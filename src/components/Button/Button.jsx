aa289a9ed1127a7826431e77c19138606715a995
Cristina Bubu <cristinaflorinamihalcea@hmail.com>
2024-07-13 03:04:14 +0300

    ookk

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import style from './Button.module.css';
+import PropTypes from "prop-types";
+
+const Button = ({onPageUpload}) => {
+    return (
+        <div>
+            <button type="button" className={style.load_btn} onClick={onPageUpload}>Load more</button>
+        </div>
+    )
+}
+
+Button.propTypes = {
+    onPageUpload: PropTypes.func.isRequired
+}
+
+export default Button;
