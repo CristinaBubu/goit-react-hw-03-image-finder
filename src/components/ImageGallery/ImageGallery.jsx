6d38f07689ff6323a817e59fca569c011c3634a2
Cristina Bubu <cristinaflorinamihalcea@hmail.com>
2024-07-13 12:33:10 +0300

    im.gal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,33 +1,26 @@
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import PropTypes from 'prop-types';
-
 import style from './ImageGallery.module.css';
-
-const ImageGallery = ({images, onModalOpen}) => {
-
-    return(
-    <ul className={style.gallery}>
-            {images.length > 0 && images.map(image => (
-                <ImageGalleryItem 
-                    key={image.id}
+const ImageGallery = ({ images, onModalOpen }) => {
+    return (
+        <ul className={style.gallery}>
+            {images.length > 0 && images.map((image, index) => (
+                <ImageGalleryItem
+                    key={`${image.id}-${index}`}
                     item={image}
                     onModalOpen={onModalOpen}
                 />
-            ))
-        }       
+            ))}
         </ul>
-    )
+    );
 }
-
-ImageGallery.prototype = {
+ImageGallery.propTypes = {
     onModalOpen: PropTypes.func.isRequired,
     images: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number.isRequired,
         webformatURL: PropTypes.string.isRequired,
         largeImageURL: PropTypes.string.isRequired,
-        tags: PropTypes.string.isRequired, 
-    }))
-}
-
-
+        tags: PropTypes.string.isRequired,
+    })).isRequired,
+};
 export default ImageGallery;
